9df07aec8638670f74cdbe21a3924054f18bea98
MariyaKorneva <85699755+MariyaKorneva@users.noreply.github.com>
2022-02-14 20:40:03 +0300

    Add files via upload

---
+++ pract2.py
@@ -0,0 +1,30 @@
+#name={'Google': 90.15, 'Yahoo!': 2.09, 'Yandex': 1.48, 'Bing': 3.23, 'other': 1.65 }
+#print(name)
+
+#today={'1': 'call my grandparents', '2': 'give valenteins card  to my friends'}
+#tomorrow={'1': 'clear the room', '2': 'feed my pets'}
+#good_deeds=[today, tomorrow]
+#print(good_deeds)
+
+#users={'1': Alex, '2': Katy, '3': Sam, '4': Klays, '5': Ann}
+#key=input("Введите число")
+#if key>=5:
+    #i==users
+    #print(f"Hi,{i}")
+#else:
+    #print(f"Hi, to all")
+
+#things={'ball': 2, 'apple': 5, 'pen': 1, 'bottle':3}
+#print(things)
+
+#num={'1': 1**1, 'n':n**n}
+#n=input("Введите число")
+#print(num)
+
+#game={'action': "StarCraft", 'simulation': "The Sims 4", 'strategy':"League of Legends",'adventure':"Avengers"}
+#print(game)
+
+name={'Brendan Eich': "04/07/1961",'Guido van Rossum': "31/01/1956",'Rasmus Lerdorf':"22/11/1968",'Martin Odersky':"05/09/1958"}
+i=input("Введите имя програмиста")
+for i,j in name():
+    print(f"{i} родился {j}")
